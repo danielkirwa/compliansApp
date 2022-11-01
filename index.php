8083ec36f4e42d568884da82cec1af3f7f226d41
danielkirwa <kirwadan03@gmail.com>
2022-11-01 11:26:00 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="styles/index.css">
+	<title></title>
+</head>
+<body>
+ <!-- start of header -->
+<div class="myheader">
+	<div class="logoholder"><img src="assets/logo.png" width="150px"></div>
+     <div class="headerdetails"> 
+     	<div class="websitename">
+          <center><h3>Kenya literature bureau</h3></center>
+     	 </div>
+     	<div class="navbar">
+           <a href="#">Register</a> &nbsp; &nbsp;
+           <a href="#">Login</a>&nbsp; &nbsp;
+     	</div>
+      </div>
+</div>
+<hr style="size: 2px;">
+<!-- end of header -->
+<!-- start of body -->
+<div class="form-holder">
+    <div class="center-card">
+         
+    </div>
+    <div class="center-card">registration</div>
+</div>
+<!-- end of body -->
+<div>footer</div>
+</body>
+</html>
