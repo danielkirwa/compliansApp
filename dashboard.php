83d9207682a604bd71f94bef3f48bb71afa7d3e0
danielkirwa <kirwadan03@gmail.com>
2022-11-15 13:54:05 +0300

    working on user dashboard

--- dashboard.php
+++ dashboard.php
@@ -48,11 +48,56 @@ if ($_SESSION['username']) {
 <!-- start of body -->
     
 <div class="dash-infor">
-    <div class="infor-card">general </div>
-     <div class="infor-card"> rates</div>
+    <div class="infor-card">
+        <div class="lb-div" style="background: dodgerblue;">All complains</div><br>
+        <div class="lb-div" style="background: tomato;">Pending</div><br>
+        <div class="lb-div" style="background:  mediumseagreen;">Closed</div>
+     </div>
+     <div class="infor-card"> 
+        <center>
+         <div class="rate-holder">
+            <br>
+             0.0%
+         </div>
+         </center>
+         <center><label style="color: mediumseagreen; font-weight:bold; font-size: 18px;">Our solve rate</label> </center>
+     </div>
 </div>
+
+<!-- your complaints -->
+
+<div>
+    <center>    <h1>Your submission</h1></center>
+
+<table id="" class="complaint-table">
+  <tr>
+    <th>Serial</th>
+    <th>Category</th>
+    <th>Date</th>
+    <th>Status</th>
+    <th>Action</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>category</td>
+    <td>1/12/2022</td>
+    <td>Recieved</td>
+    <td><button class="view-btn">View</button> </td>
+  </tr>
+  <tr>
+     <td>2</td>
+    <td>category</td>
+    <td>1/22/2022</td>
+    <td>Recieved</td>
+    <td><button class="view-btn">View</button> </td>
+  </tr>
+  
+ 
+</table>
+    </div>
    
 
+<br><br><br>
 
 <!-- end of body -->
 <div class="footer">
